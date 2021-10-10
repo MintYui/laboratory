9e6c07e2eaad0536e1ba4463ab673bd6b9bce398
Roman <60356431+MintYui@users.noreply.github.com>
2021-10-10 20:25:28 +0300

    Update lab5.cpp

--- lab/lab5/lab5.cpp
+++ lab/lab5/lab5.cpp
@@ -4,38 +4,6 @@ using namespace std;
 
 double enterAndCheckNum();
 
-int main() {
-	int i, j = 0, g = 0, n, sum = 0, choice;
-	do {
-		cout << "Enter array size from 1 to 100\t";
-		n = enterAndCheckNum();
-	} while (n < 1 || n>100);
-
-	int* a = new int[n];
-
-	do {
-		cout << "Do you want to use random numbers or enter them yourself ? \t 1 - random 2 - enter\n";
-		choice = enterAndCheckNum();
-	} while (choice != 1 && choice != 2);
-
-	if (choice == 2) {
-		for (i = 0; i < n; ++i)
-			cin >> a[i];
-	}
-	else {
-		srand(time(0));
-		for (i = 0; i < n; ++i) {
-			a[i] = rand() % 15 - 10;
-			cout << a[i] << " ";
-		}
-	}
-
-	for (i = 0; i < n; i++) {
-		if (a[i] > 0) {
-			j = i;
-			break;
-		}
-	}
 	for (i = n - 1; i >= j; i--) {
 		if (a[i] > 0) {
 			g = i;
